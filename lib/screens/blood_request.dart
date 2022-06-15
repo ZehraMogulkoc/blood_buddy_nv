1bca520877ede138151afedeb22a1184b6c7c0a0
Zehra Moğulkoç <87859856+ZehraMogulkoc@users.noreply.github.com>
2022-06-15 15:46:30 +0300

    Update blood_request.dart

--- lib/screens/blood_request.dart
+++ lib/screens/blood_request.dart
@@ -1,107 +1,144 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/material.dart';
-import 'package:blood_buddy_nv/screens/loading.dart';
+import 'dart:convert';
 
-class ShowRequests extends StatelessWidget {
+import 'package:blood_buddy_nv/screens/donor_drawet.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class ShowRequest extends StatefulWidget {
+  @override
+  _ShowRequestState createState() => _ShowRequestState();
+}
+List requestList= [];
+List requestBloodType = [];
+
+class _ShowRequestState extends State<ShowRequest> {
+
+
+//with http class, connect to php files and reach the all Data class
+  getAllRequest()async{
+    Uri url_request = Uri.parse("http://192.168.1.30/bloodbuddy/allRequestAdress.php");
+    var response=await http.get(url_request);
+    if(response.statusCode == 200){
+      setState(() {
+        requestList = json.decode(response.body);
+        print(requestList);
+      });
+      print(requestList);
+      return requestList;
+    }
+  }
+
+  getAllRequestBloodType()async{
+    Uri url_requestBloodType = Uri.parse("http://192.168.1.30/bloodbuddy/allRequest.php");
+    var response=await http.get(url_requestBloodType);
+    if(response.statusCode == 200){
+      setState(() {
+        requestBloodType = json.decode(response.body);
+        print(requestBloodType);
+      });
+      print(requestBloodType);
+      return requestBloodType;
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getAllRequest();
+    getAllRequestBloodType();
+  }
+
+  //final bloodRequestRef = Firestore.instance.collection('request');
 
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 2.0,
-      child: Container(
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(50.0)
+    return Scaffold(
+      drawer: DonorDrawer(),
+        appBar: AppBar(
+          backgroundColor: Color.fromRGBO(
+              171, 39, 39, 1.0),
+          title: Text("Blood Requests"),
         ),
-        padding: EdgeInsets.only(bottom: 10.0),
-        child: Row(
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(5.0),
-              child: Stack(
-                children: <Widget>[
-                  Container(
-                      height: 120.0,
-                      width: 100.0,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        body: ListView.builder(
+          itemCount: requestList.length,
+          itemBuilder: (context,index){
+            return Card(
+              elevation: 2.0,
+              child: Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50.0)
+                ),
+                padding: EdgeInsets.only(bottom: 10.0),
+                child: Row(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(5.0),
+                      child: Stack(
                         children: <Widget>[
-                          Expanded(
-                            flex: 2,
-                            child: Container(
-                              color: Colors.black87,
-                              alignment: Alignment.center,
-                              child: Text('Location', style: TextStyle(color: Colors.white, )),
-                            ),
+                          Container(
+                              height: 120.0,
+                              width: 100.0,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                children: <Widget>[
+                                  Expanded(
+                                    flex: 2,
+                                    child: Container(
+                                      color: Colors.black87,
+                                      alignment: Alignment.center,
+                                      child: Text(requestList[index]["address"], style: TextStyle(color: Colors.white, )),
+                                    ),
+                                  ),
+                                  Expanded(
+                                    flex: 3,
+                                    child:  Container(
+                                      alignment: Alignment.center,
+                                      color: Colors.red,
+                                      child: Text(requestBloodType[index]["blood_type"], style: TextStyle(color: Colors.white, fontSize: 20.0, fontWeight: FontWeight.bold)),
+                                    ),
+                                  )
+                                ],
+                              )
                           ),
-                          Expanded(
-                            flex: 3,
-                            child:  Container(
-                              alignment: Alignment.center,
-                              color: Colors.red,
-                              child: Text('bloodGroup', style: TextStyle(color: Colors.white, fontSize: 20.0, fontWeight: FontWeight.bold)),
-                            ),
-                          )
                         ],
-                      )
-                  ),
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.symmetric(vertical: 0.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Container(
-                          padding: EdgeInsets.symmetric(vertical: 10.0),
-                          child: Text(
-                            "Required in particular date",
-                            style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 20.0,
-                                fontFamily: "Gotham",
-                                fontWeight: FontWeight.bold),
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                        ),
-                        Row(
-                          children: <Widget>[
-                            Icon(Icons.hdr_strong, color: Colors.redAccent,),
-                          ],
-                        ),
-                        SizedBox(height: 8.0,),
-                        Row(
-                          children: <Widget>[
-                            Icon(Icons.phone, color: Colors.blue,),
-                            InkWell(
-                              onTap: (){
-
-                              },
-                              child: Text(
-                                "Call Now",
-                                style: TextStyle(color: Colors.black87, fontWeight: FontWeight.bold, fontFamily: "Gotham", fontSize: 18.0),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
+                      ),
                     ),
-                  ),
-                ],
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Padding(
+                            padding: EdgeInsets.symmetric(vertical: 0.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Row(
+                                  children: <Widget>[
+                                    //Icon(Icons.hdr_strong, color: Colors.redAccent,)
+                                    Text( requestBloodType[index]["amount"]  ?? ' '+
+                                        "pin Blood Needed",
+                                      style: TextStyle(color: Colors.black87, fontWeight: FontWeight.bold, fontFamily: "Gotham", fontSize: 18.0),
+                                    ),
+                                  ],
+                                ),
+                                SizedBox(height: 8.0,),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
               ),
-            )
-          ],
-        ),
-      ),
+            );
+          },
+        )
     );
   }
 }
